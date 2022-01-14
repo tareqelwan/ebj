02c7aa24775aa10bc9311cf902eab0d40d41c960
Tarek <tareqelwan@gmail.com>
2022-01-15 01:37:27 +0200

    base.html

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
+# important for static files
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customers/',include('customers.urls')),
     path('setup/',include('setup.urls')),
     
 ]
+
+#static urls
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#media urls
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
